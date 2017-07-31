51851ad1fbd5070d57a03e09931727c269cb9c23
gtbmed <jkbarson@gmail.com>
2017-07-30 21:37:20 -0400

    Challenge #1 Minimum Viable Product

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,6 +16,14 @@ const connection = mysql.createConnection({
 connection.connect(function(error) {
   if (error) throw error;
   console.log("connected as id " + connection.threadId);
+});
+start();
+
+function updateStock(itemID, countChange) {
+  connection.query("UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?", [countChange, itemID], function(error) {
+  if (error) throw error;
+  console.log("Transaction Complete");
+  });
 };
 
 function start() {
@@ -23,15 +31,31 @@ function start() {
     for (var i = 0; i < res.length; i++) {
       console.log("Item ID: " + res[i].item_id + " || Product Name: " + res[i].product_name + " || Price: $" + res[i].price);
     }
-  }
-  inquirer
-    .prompt([
+  })
+  inquirer.prompt([
       {
-        type: input,
-        name: customerSelect,
-        message: "Welcome to BAMAZON.  Please enter the item id of the product you would like to purchase."
+        type: "input",
+        name: "customerSelect",
+        message: "\nWelcome to BAMAZON.  Please enter the item id of the product you would like to purchase.\n"
+      },
+      {
+        type: "input",
+        name: "customerQuantity",
+        message:  "How many of this item would you like to purchase?"
       }
-    ])
-}
-
-start();
+    ]).then(function(answer){
+      connection.query("SELECT stock_quantity FROM products WHERE item_id = ?", [answer.customerSelect], function(error, res) {
+        if (error) throw error;
+        console.log(res);
+        if (res[0].stock_quantity >= parseInt(answer.customerQuantity)) {
+          // connection.query("UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?"), [answer.customerQuantity, answer.customerSelect]
+          updateStock(answer.customerSelect, answer.customerQuantity);
+          start();
+        }
+        else {
+          console.log("Insufficient Quantity");
+          start();
+        }
+      });
+    });
+  };
